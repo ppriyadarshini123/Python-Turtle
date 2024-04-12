257718f01746519b900103f41814be1c46a9e3d3
ppriyadarshini123 <payal.priyadarshini@gmail.com>
2024-04-12 15:26:45 +0100

    Python Turtle practice

---
+++ main.py
@@ -0,0 +1,38 @@
+#Ctrl+/ to toggle comment
+from turtle import *
+import time
+# forward(50)
+# penup()
+# right(90)
+# forward(100)
+# right(90)
+# pendown()
+# forward(50)
+# hideturtle()
+
+# #Draw a square
+# for i in range(4):
+#     forward(100)
+#     left(90)
+# hideturtle()
+
+#Draw a circle
+# circle(100)
+# bgcolor("Green")
+
+#Fill a shape with colour
+# fillcolor("Yellow")
+# begin_fill()
+# circle(100)
+# end_fill()
+# hideturtle()
+
+# Two ways to style a pen
+pensize(20)
+pencolor("Magenta")
+circle(100)
+hideturtle()
+#reset()
+#clear()
+
+time.sleep(10)
